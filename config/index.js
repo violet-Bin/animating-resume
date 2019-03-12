ccc35deb96a2c29d43176929d27a3113657a7584
jiangjiabin3 <jiangjiabin3@jd.com>
2019-03-12 11:08:23 +0800

    upsate

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 20111,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
